ab206a4a8ea00aa008f68c29e59212449ba776bf
linsjulia <julia.silva465@aluno.cps.sp.gov.br>
2026-08-24 10:26:03 -0300

    refactor: modifica informações do html

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 // Carregar modulos
 const http = require("http");
 const url = require("url");
@@ -7,33 +6,34 @@ const fs = require("fs");
 
 function readFile(response, file){
     fs.readFile(file, function(err, data){
-        response.end(data)
-    })
+        response.end(data);
+    });
 }
 
 
 var callback = function(request, response){
-    response.writeHead(200, {"Content-type": "text-plain"});
-
     var parts = url.parse(request.url);
 
     if(parts.path == "/"){
-        response.writeHead(200, {"Content-type": "text-plain"});
+        response.writeHead(200, {"Content-type": "text/html; charset=utf-8"});
         readFile(response, "index.html");
+    } 
+    else if(parts.path == "/Julia"){
+        response.writeHead(200, {"Content-type": "text/html; charset=utf-8"});
+        readFile(response, "index.html");
+    } 
+    else if(parts.path == "/Vitor"){
+        response.writeHead(200, {"Content-type": "text/html; charset=utf-8"});
+        readFile(response, "index.html");
+    } 
+    else {
+        response.writeHead(404, {"Content-type": "text/plain; charset=utf-8"});
+        response.end("Página não encontrada!");
     }
-    // } else if(parts.path == "/Julia"){
-    //     response.writeHead(200, {"Content-type": "text-plain"});
-    //     readFile(response, "index.html")
-
-    // } else if(parts.path == "/Vitor"){
-    //     response.writeHead(200, {"Content-type": "text-plain"});
-    //     readFile(response, "index.html")
-    // }
-
-}
+};
 
 
 // Criar e configurar um servidor http
-var server = http.createServer(callback)
-server.listen(3000)
-console.log("Servidor iniciando na porta: http://localhost:3000/")
+var server = http.createServer(callback);
+server.listen(3000);
+console.log("Servidor iniciando na porta: http://localhost:3000/");
