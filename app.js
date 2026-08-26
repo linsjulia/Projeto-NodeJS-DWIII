8d1c92fc0b97caa4a941ce9a866213e75523ab1f
linsjulia <julia.silva465@aluno.cps.sp.gov.br>
2026-08-25 23:49:19 -0300

    feat: adiciona rotas, paginas html e arquivos pdf  e atualiza logica de app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require("http");
 const url = require("url");
 const fs = require("fs");
 
-
+// Módulo que lê arquivos
 function readFile(response, file){
     fs.readFile(file, function(err, data){
         response.end(data);
@@ -11,21 +11,52 @@ function readFile(response, file){
 }
 
 
+// Função Callback
 var callback = function(request, response){
     var parts = url.parse(request.url);
 
+    // Rota principal do projeto
     if(parts.path == "/"){
         response.writeHead(200, {"Content-type": "text/html; charset=utf-8"});
         readFile(response, "index.html");
     } 
-    else if(parts.path == "/Julia"){
+
+    // Rotas de apresentação: Júlia
+    else if(parts.path == "/julia"){
         response.writeHead(200, {"Content-type": "text/html; charset=utf-8"});
-        readFile(response, "index.html");
+        readFile(response, "julia/index.html");
     } 
-    else if(parts.path == "/Vitor"){
+    else if(parts.path == "/julia/sobre"){
         response.writeHead(200, {"Content-type": "text/html; charset=utf-8"});
-        readFile(response, "index.html");
+        readFile(response, "julia/sobre/sobre.html");
     } 
+    else if(parts.path == "/julia/curriculo"){
+        response.writeHead(200, {"Content-type": "application/pdf"});
+        readFile(response, "julia/curriculo/curriculo.pdf");
+    }
+
+    // Rotas de apresentação: Vitor
+    else if(parts.path == "/vitor"){
+        response.writeHead(200, {"Content-type": "text/html; charset=utf-8"});
+        readFile(response, "vitor/index.html");
+    } 
+
+    else if(parts.path == "/vitor/sobre"){
+        response.writeHead(200, {"Content-type": "text/html; charset=utf-8"});
+        readFile(response, "vitor/sobre/sobre.html");
+    } 
+    else if(parts.path == "/vitor/curriculo"){
+        response.writeHead(200, {"Content-type": "text/html; charset=utf-8"});
+        readFile(response, "vitor/curriculo/curriculo.pdf");
+    }
+    
+    // Rota de apresentação do projeto
+    else if (parts.path == "/projeto"){
+        response.writeHead(200, {"Content-type": "application/pdf"})
+        readFile(response, "projeto/documentacao.pdf")
+    }
+
+    // Caso não tenha sido encontrada nenhuma rota
     else {
         response.writeHead(404, {"Content-type": "text/plain; charset=utf-8"});
         response.end("Página não encontrada!");
